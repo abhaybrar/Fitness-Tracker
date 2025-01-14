b145f0e3fda982ad0200dc1acbfac76db504230c
Abhay Brar <38541606+abhaybrar@users.noreply.github.com>
2025-01-14 12:55:17 -0500

    Add files via upload

---
+++ Fitness Tracker/config.py
@@ -0,0 +1,8 @@
+# config.py
+DATABASE_CONFIG = {
+    'host': '12.4',  # localhost
+    'port': 3306,  # DBngin MySQL port (default is 3306)
+    'user': 'root',  # Default MySQL user
+    'password': '',  # No password, so leave this as an empty string
+    'database': 'fitness_data'  # Name of the database you're using
+}
